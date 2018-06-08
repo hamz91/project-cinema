63ffd28e7c9703e8a9f2b086b694704b32f911a3
Hamzah Kurdi <hamzahkurdi@aol.com>
2018-06-08 11:40:55 +0100

    added fetch request and first 2 'then' functions

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const button = document.querySelector("#movie_submit");
 // Form event listener
 form.addEventListener("submit", function(event) {
   // Prevent default server form submission
-  event.preventDefault;
+  event.preventDefault();
 
   // Search input string
   let movie = textArea.value;
+  console.log(movie);
+
+  // Empty Text Area
+  textArea.value = "";
+
+  //   Fetch request using Text Area string
+  fetch(`http://www.omdbapi.com/?s=${movie}&apikey=77c3b516`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      return myJson;
+    })
+    .then(function())
 });
